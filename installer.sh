3c6aa055fb771387635283ecff5d5634356ed4dc
Vyshantha <yshant@gmail.com>
2021-03-21 14:51:12 +0100

    Linguistic Info, Dictionary, Thesaurus rendering options

--- installer.sh
+++ installer.sh
@@ -3,14 +3,8 @@
 npm install
 npm i -g nodemon
 npm install -g "@angular/cli"
-cd client/
+cd editorClient
 npm install
 cd ..
 ./startSVA.sh # permission for executing startSVA.sh or startSVA.bat
 # http://hostname:4200/sva (Soil Viewer)
-
-# Add new / edit existing Soil Fact-sheet
-# Token-based authentication
-# Publically accessible URL
-# Print solution
-# Make "District" logic dynamic in list view
