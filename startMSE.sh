4b06fca6161a72be3ad98c430cac6fe703dfd90e
Vyshantha Simha <yshant@gmail.com>
2022-01-07 13:01:49 +0100

    Update startMSE.sh

--- startMSE.sh
+++ startMSE.sh
@@ -8,9 +8,13 @@ HOSTNAME=`ifconfig -a | grep -i "inet " | awk 'NR==2{print $2}'`
 
 # Update Environment file
 echo $HOSTNAME
-sed -i -e 's/^{"hostname" : ".*",/{"hostname" : "'$HOSTNAME'",/g' ./routes/environments/sva_config.json
+sed -i -e 's/^{"hostname" : ".*", "port" : 443, "clientPort": 443,/{"hostname" : "'$HOSTNAME'", "port" : 5555, "clientPort": 4200,/g' ./routes/environments/sva_config.json
 rm -rf ./routes/environments/sva_config.json-e
-sed -i -e 's/^{"hostname" : ".*",/{"hostname" : "'$HOSTNAME'",/g' ./editorClient/src/assets/environments/sva_config.json
+sed -i -e 's/^app.hostname=.*/app.hostname='$HOSTNAME'/g' ./routes/environments/sva_config.properties
+sed -i -e 's/^app.port=443/app.port=5555/g' ./routes/environments/sva_config.properties
+sed -i -e 's/^angular.port=443/angular.port=4200/g' ./routes/environments/sva_config.properties
+rm -rf ./routes/environments/sva_config.properties-e
+sed -i -e 's/^{"hostname" : ".*", "port" : 443, "clientPort": 443,/{"hostname" : "'$HOSTNAME'", "port" : 5555, "clientPort": 4200,/g' ./editorClient/src/assets/environments/sva_config.json
 rm -rf ./editorClient/src/assets/environments/sva_config.json-e
 
 cd editorClient
