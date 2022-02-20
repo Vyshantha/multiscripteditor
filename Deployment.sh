d69a1dbf4fb67dd73fb7d17363def90329395f86
Vyshantha Simha <yshant@gmail.com>
2022-02-20 11:14:46 +0100

    Heroku required Python updates
    https://github.com/Vyshantha/multiscripteditor/issues/64

--- Deployment.sh
+++ Deployment.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 ### Server-Deploy : Heroku
-### Push - Github Integration Deploy https://dashboard.heroku.com/apps/worldscriptsexplorer/deploy/github
 ### Manual Deployment - Remove SSL instances in tasks.js before deployment
 cd /Users/yshant/Documents/MSE_server_deploy
 cp ./routes/environments/sva_config_prod.json ./routes/environments/sva_config.json
@@ -9,13 +8,19 @@ cp ./routes/environments/sva_config_prod.properties ./routes/environments/sva_co
 curl https://cli-assets.heroku.com/install.sh | sh # ONLY first time
 heroku login # ONLY first time
 git init # ONLY first time
-# Python and Pip package setup
-curl -o get-pip.py https://bootstrap.pypa.io/get-pip.py # ONLY first time
-python get-pip.py  # ONLY first time
 heroku git:remote -a worldscriptsexplorer
 git add .
 git commit -am "*-WSEv1"
 git push heroku master
+## Currently using Manual Github Integration Deploy : https://dashboard.heroku.com/apps/worldscriptsexplorer/deploy/github
+
+# Python and Pip package setup on Heroku  ### ONLY first time
+cat /etc/os-release
+curl -o get-pip.py https://bootstrap.pypa.io/get-pip.py
+python get-pip.py
+wget http://archive.ubuntu.com/ubuntu/pool/universe/p/python-pip/python3-pip_20.0.2-5ubuntu1_all.deb
+dpkg -i python3-pip_20.0.2-5ubuntu1_all.deb
+apt-get install -f ./python3-pip_20.0.2-5ubuntu1_all.deb
 
 ### Client-Deploy - Firebase Deploy https://console.firebase.google.com/u/0/project/worldscriptsexplorer/hosting/sites
 cd /Users/yshant/Documents/GitHub/multiscripteditor/editorClient
